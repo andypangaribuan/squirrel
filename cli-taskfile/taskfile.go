55f63d2cccdfd0c0cbc2815cdd6e1966ec13ee37
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2025-06-23 15:28:09 +0700

    update

--- cli-taskfile/taskfile.go
+++ cli-taskfile/taskfile.go
@@ -14,6 +14,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"squirrel/app"
 	"squirrel/util"
 	"strings"
 
@@ -22,28 +23,57 @@ import (
 )
 
 func Exec() {
-	filePath := fmt.Sprintf("%v/.taskfile", getWorkingDirectory())
+	var (
+		filePaths = []string{fmt.Sprintf("%v/.taskfile", getWorkingDirectory())}
+		args      = app.Args.Args()
+		model     = &stuTaskfile{
+			items:            make([][]any, 0),
+			newLineAtIndexes: make([]int, 0),
+		}
+	)
+
+	if len(args) > 1 {
+		for i, arg := range args {
+			if arg == "taskfile" {
+				continue
+			}
+
+			if i == 1 && arg != ".taskfile" {
+				filePaths[0] = arg
+			} else {
+				filePaths = append(filePaths, arg)
+			}
+		}
+	}
+
+	for _, filePath := range filePaths {
+		err := fileOutput(filePath, model)
+		if err != nil {
+			return
+		}
+	}
+
+	printOutput(model)
+}
+
+func fileOutput(filePath string, model *stuTaskfile) error {
 	_, err := os.Stat(filePath)
 	if errors.Is(err, os.ErrNotExist) {
-		return
+		return err
 	}
 
 	content, err := os.ReadFile(filePath)
 	if err != nil {
 		fmt.Printf("error: %v\n%+v\n\n", err, err)
-		return
+		return err
 	}
 
-	var (
-		lines            = strings.Split(string(content), "\n")
-		items            = make([][]any, 0)
-		newLineAtIndexes = make([]int, 0)
-	)
+	lines := strings.Split(string(content), "\n")
 
 	for i, line := range lines {
 		line = strings.TrimSpace(line)
 		if line == "#: space" {
-			newLineAtIndexes = append(newLineAtIndexes, len(items)-1)
+			model.newLineAtIndexes = append(model.newLineAtIndexes, len(model.items)-1)
 			continue
 		}
 
@@ -55,7 +85,7 @@ func Exec() {
 					if len(ls) == 3 {
 						desc := strings.TrimSpace(line[3:])
 						function := ls[1] + " "
-						items = append(items, []any{"", function, desc})
+						model.items = append(model.items, []any{"", function, desc})
 					}
 				}
 			}
@@ -63,33 +93,44 @@ func Exec() {
 		}
 	}
 
-	if len(items) > 0 {
-		output := util.Build(items)
-		if len(newLineAtIndexes) > 0 {
-			lines := strings.Split(output, "\n")
-			newLines := make([]string, 0)
+	return nil
+}
 
-			if fm.IfHaveIn(-1, newLineAtIndexes...) {
-				newLines = append(newLines, "")
-			}
+func printOutput(model *stuTaskfile) {
+	if len(model.items) == 0 {
+		return
+	}
 
-			for i, line := range lines {
-				newLines = append(newLines, line)
-				if fm.IfHaveIn(i, newLineAtIndexes...) {
-					newLines = append(newLines, "")
-				}
-			}
+	output := util.Build(model.items)
+	if len(model.newLineAtIndexes) > 0 {
+		lines := strings.Split(output, "\n")
+		newLines := make([]string, 0)
 
-			output = strings.Join(newLines, "\n")
+		if fm.IfHaveIn(-1, model.newLineAtIndexes...) {
+			newLines = append(newLines, "")
 		}
 
-		msg := stringFormatter.FormatComplex(`
+		for i, line := range lines {
+			newLines = append(newLines, line)
+			if fm.IfHaveIn(i, model.newLineAtIndexes...) {
+				newLines = append(newLines, "")
+			}
+		}
+
+		output = strings.Join(newLines, "\n")
+	}
+
+	msg := stringFormatter.FormatComplex(`
 {commands}
 {output}`,
-			map[string]any{
-				"commands": util.ColorBoldGreen("commands:"),
-				"output":   output,
-			})
-		fmt.Println(msg)
+		map[string]any{
+			"commands": util.ColorBoldGreen("commands:"),
+			"output":   output,
+		})
+
+	if len(msg) > 1 && msg[:1] == "\n" {
+		msg = msg[1:]
 	}
+
+	fmt.Println(msg)
 }
